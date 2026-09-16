8f5d0483b2d6243a9aebaf62ad914f0f3b79c71e
Gustavo De Souza Calixto <00001120133610sp@aluno.educacao.sp.gov.br>
2026-09-16 08:52:32 -0300

    feat: adicionar multiplicacao e divisao

--- script.js
+++ script.js
@@ -33,3 +33,25 @@ function dividir() {
 
   document.getElementById("resultado").innerHTML = "Resultado: " + resultado;
 }
+function calcularTudo() {
+  let numero1 = Number(document.getElementById("numero1").value);
+  let numero2 = Number(document.getElementById("numero2").value);
+
+  let soma = numero1 + numero2;
+  let subtracao = numero1 - numero2;
+  let multiplicacao = numero1 * numero2;
+  
+  let divisao = "";
+  if (numero2 === 0) {
+      divisao = "Não é possível dividir por zero";
+  } else {
+      divisao = numero1 / numero2;
+  }
+
+  document.getElementById("resultado-tudo").innerHTML = `
+      Soma: ${numero1} + ${numero2} = ${soma} <br>
+      Subtração: ${numero1} - ${numero2} = ${subtracao} <br>
+      Multiplicação: ${numero1} * ${numero2} = ${multiplicacao} <br>
+      Divisão: ${numero1} / ${numero2} = ${divisao}
+  `;
+}
